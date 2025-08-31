3113149b119ac5bb8bd2bb3a4e034abd7fdd921b
Zeyad Khaled Ezzat <zkhaled389@gmail.com>
2025-08-31 13:38:38 -0400

    constructor types

--- MyProject1/OOP/Person.cs
+++ MyProject1/OOP/Person.cs
@@ -16,13 +16,43 @@ namespace OOP //organizer for classes
         public string Name { get; set; } //auto-implemented property
         public int Age { get; private set; } //read-only property outside the class
         //constructors
-        //we already have default constructor (no parameters) , no need to write it
-       //parameterized constructor
+        //we already have 1-default constructor (no parameters) , no need to write it
+        public Person() { } //default constructor
+
+        // 2-Private constructor prevents direct object creation
+        // Ensures class can't be instantiated from outside
+        // Commonly used in Singleton or Factory pattern
+        // Forces object creation to go through this method
+        // Compiler makes constructor private automatically for static classes
+        
+        //3-parameterized constructor
         public Person(string name, int age, string ssn) //constructor with parameters
         {
             Name = name;
             Age = age;
             this.ssn = ssn; //this keyword to refer to the current instance of the class
+        }
+        //4-copy constructor is creating a new object by taking a copy from another object
+        public Person(Person p)
+        {
+            this.Name = p.Name;
+            this.Age = p.Age;
+            this.ssn = p.ssn;
+
+        }
+        //5- copy constructors 
+        // Static constructor:
+        // - Runs only once, before the class is used (first access).
+        // - Used to initialize static fields.
+        // - Cannot have parameters.
+        // - Cannot be called manually (runtime calls it automatically).
+        // - Executes before any instance constructor or static method is accessed.
+
+        static Person() { 
+            
+        
+        
+        
         }
         //methods
         //instance method
@@ -66,5 +96,10 @@ namespace OOP //organizer for classes
             num += 10;
             return num ;
         }
+        public static void calc(int num, out int res)  //passed by reference
+        {
+            res = num + 10;
+        }
+        
     }
 }
--- MyProject1/OOP/Program.cs
+++ MyProject1/OOP/Program.cs
@@ -3,13 +3,19 @@ using OOP;
 
 Console.WriteLine("----------OOP---------------");
 Person p = new Person("John", 30, "123-45-6789");
-Console.WriteLine(p.GetData());
+//Console.WriteLine(p.GetData());
 //p.show();
-Console.WriteLine(Person.DispDate());  //calling static method without an object
-Console.WriteLine(p.GetData(true)); //calling overloaded method
+//Console.WriteLine(Person.DispDate());  //calling static method without an object
+//Console.WriteLine(p.GetData(true)); //calling overloaded method
 
-await Person.Task2(); //calling asynchronus method 
+//await Person.Task2(); //calling asynchronus method 
 
 int num = 10;
 Person.Add(ref num); //passed by reference
-Console.WriteLine(num);
+//Console.WriteLine(num);
+Person.calc(num,out int result);
+//Console.WriteLine(result);
+
+Person CopyPerson = new Person(p);
+Console.WriteLine(CopyPerson.GetData());
+
